c4e0aa54c36ca15377a32c40be8c497d66288658
dannypepper <bors.dani@gmail.com>
2022-06-02 19:59:49 +0200

    Merge conflicts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-<<<<<<< HEAD
   <NavBar />
-=======
->>>>>>> cd83825f8da869184f7b3bbd3d78d5a9f805ce31
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-<<<<<<< HEAD
 import NavBar from './components/NavBar.vue';
-=======
->>>>>>> cd83825f8da869184f7b3bbd3d78d5a9f805ce31
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-<<<<<<< HEAD
     NavBar,
-=======
->>>>>>> cd83825f8da869184f7b3bbd3d78d5a9f805ce31
     HelloWorld,
   },
 };
